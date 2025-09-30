d15094e577399586fcb511ef655b4b70e079a667
Mananas <Mananas@host.docker.internal>
2025-09-30 13:37:50 +0200

    sdf

--- src/main/java/es/cursogetafe/springdata/persistencia/AsignaturaDao.java
+++ src/main/java/es/cursogetafe/springdata/persistencia/AsignaturaDao.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import es.cursogetafe.springdata.modelo.Asignatura;
+import es.cursogetafe.springdata.modelo.Profesor;
 
 public interface AsignaturaDao extends JpaRepository<Asignatura, Integer>{
 
@@ -15,5 +16,10 @@ public interface AsignaturaDao extends JpaRepository<Asignatura, Integer>{
 	
 	@Query("select a from Asignatura a where a.asignatura like %?1% or a.facultad like %?1%")
 	Set<Asignatura> buscarPorDescripcion(String desc);
+	
+	
+	
+	Set<Asignatura> findByProfesor(Profesor profesor);
+	
 	
 }
